1cd7da56e7e9a2f059c18e18948e76b691a597d0
Sean Raymor <sraymor17@gmail.com>
2024-09-18 13:32:27 -0400

    updated error messages

--- matchup_data/convert_html_to_csv.py
+++ matchup_data/convert_html_to_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 # User Input
-WEEK = 2
+WEEK = 1
 
 class Player:
     def __init__(self, name, position, fan_pts):
@@ -12,21 +12,24 @@ class Player:
         self.fan_pts = fan_pts
 
 def convert_league_matchup_table_to_csv():
-    with open(F'matchup_data/week{WEEK}/week{WEEK}_matchups.html') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+    try:
+        with open(F'matchup_data/week{WEEK}/week{WEEK}_matchups.html') as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
 
-    matchup_df = pd.DataFrame(columns=["Team1", "Team1 Score", "Team2", "Team2 Score", "Winner"])
+        matchup_df = pd.DataFrame(columns=["Team1", "Team1 Score", "Team2", "Team2 Score", "Winner"])
 
-    for matchup in soup.find_all('li'):
-        team1 = matchup.find_all('a')[1].text
-        team1_score = float(matchup.find_all('div')[11].text)
-        team2 = matchup.find_all('a')[4].text
-        team2_score = float(matchup.find_all('div')[18].text)
-        winner = team2 if team2_score > team1_score else team1
-        row = [team1, team1_score, team2, team2_score, winner]
-        matchup_df.loc[len(matchup_df)] = row
+        for matchup in soup.find_all('li'):
+            team1 = matchup.find_all('a')[1].text
+            team1_score = float(matchup.find_all('div')[11].text)
+            team2 = matchup.find_all('a')[4].text
+            team2_score = float(matchup.find_all('div')[18].text)
+            winner = team2 if team2_score > team1_score else team1
+            row = [team1, team1_score, team2, team2_score, winner]
+            matchup_df.loc[len(matchup_df)] = row
 
-    matchup_df.to_csv(f"matchup_data/week{WEEK}/matchup.csv")
+        matchup_df.to_csv(f"matchup_data/week{WEEK}/matchup.csv")
+    except:
+        print(f"Week {WEEK} matchups html not available")
 
 def clean_matchup_df(df):
     df.drop(['Stats', 'Stats.1'], axis=1, inplace=True)
@@ -36,10 +39,8 @@ def clean_matchup_df(df):
 
 def convert_detailed_matchup_to_csv():
     for i in range(1,7):
-        current_directory = os.getcwd()
-        entries = os.listdir(current_directory)
         if os.path.exists(f"matchup_data/week{WEEK}/matchup_{i}.csv"):
-            print(f"csv for matchup {i} already exists")
+            print(f"csv for week {WEEK} matchup {i} already exists")
             continue
         else:
             try:
@@ -57,7 +58,7 @@ def convert_detailed_matchup_to_csv():
                 combined_df = combined_df.rename(columns={"Player": team1, "Player.1": team2})
                 combined_df[[team1, "Fan Pts", "Pos", "Fan Pts.1", team2]].to_csv(f"matchup_data/week{WEEK}/matchup_{i}.csv")
             except:
-                print(f"Matchup {i} not available")
+                print(f"Week {WEEK} matchup {i} html not available")
 
 def main():
     convert_league_matchup_table_to_csv()
