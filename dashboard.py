0a35f9e537ccf25df90fe86b233cdee361bbdc01
Sean Raymor <sraymor17@gmail.com>
2024-10-03 10:05:54 -0400

    moved func

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,27 @@
 import streamlit as st
-from convert_html_to_csv import convert_league_matchup_table_to_df, convert_detailed_matchup_to_df, League
+from convert_html_to_csv import convert_league_matchup_table_to_df, convert_detailed_matchup_to_df, League, Roster, MatchUp
+from bs4 import BeautifulSoup
 
-
+def convert_detailed_matchup_to_df_test(week, i):
+    '''covert each matchup (matchup_{i}.html) to a user friendly csv table'''
+    try:
+        with open(F'matchup_data/week{week}/matchup_{i}.html') as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
+        matchup_header = soup.find("section", {"id": "matchup-header"})
+        team1 = matchup_header.find_all('div')[6].text
+        team2 = matchup_header.find_all('div')[19].text
+        print(team1, team2)
+        print(f'matchup_data/week{week}/matchup_{i}.html')
+        matchup_df = pd.read_html(f'matchup_data/week{week}/matchup_{i}.html')
+        print(matchup_df[1].head())
+        team_1_roster = Roster(team1, matchup_df[1].iloc[:,1:4], matchup_df[2].iloc[:,1:4])
+        team_2_cols = ["Player.1", "Proj.1", "Fan Pts.1"]
+        team_2_roster = Roster(team2, matchup_df[1][team_2_cols], matchup_df[2][team_2_cols])
+        matchup = MatchUp(team_1_roster, team_2_roster)
+        return matchup
+    except:
+        print("issue creating df for week", week, " matchup", i)
+        return
 def get_teams_from_league_summary(league_summary):
     left_teams = league_summary["Team1"].tolist()
     right_teams = league_summary["Team2"].tolist()
@@ -47,7 +67,7 @@ rosters = []
 
 for i in range(1,7):
     # try:
-    matchup = convert_detailed_matchup_to_df(week, i)
+    matchup = convert_detailed_matchup_to_df_test(week, i)
     print(matchup)
     rosters.append(matchup.team1_roster)
     rosters.append(matchup.team2_roster)
