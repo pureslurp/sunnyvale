a4e07bf6f49de51f8be126eb52704cdb51191925
Sean Raymor <sraymor17@gmail.com>
2024-10-15 09:44:06 -0400

    fixed bug

--- convert_html_to_csv.py
+++ convert_html_to_csv.py
@@ -11,12 +11,12 @@ def convert_league_matchup_table_to_df(week) -> None | pd.DataFrame:
             soup = BeautifulSoup(fp, 'html.parser')
 
         matchup_df = pd.DataFrame(columns=["Team1", "Team1 Score", "Team2", "Team2 Score", "Winner"])
-
-        for matchup in soup.find_all('li'):
-            team1 = matchup.find_all('a')[1].text
-            team1_score = float(matchup.find_all('div')[11].text)
-            team2 = matchup.find_all('a')[4].text
-            team2_score = float(matchup.find_all('div')[18].text)
+        matchup_data = soup.find_all('li')
+        for i in range(5):
+            team1 = matchup_data[i].find_all('a')[1].text
+            team1_score = float(matchup_data[i].find_all('div')[11].text)
+            team2 = matchup_data[i].find_all('a')[4].text
+            team2_score = float(matchup_data[i].find_all('div')[18].text)
             winner = team2 if team2_score > team1_score else team1
             row = [team1, team1_score, team2, team2_score, winner]
             matchup_df.loc[len(matchup_df)] = row
