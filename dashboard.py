f03888cccc7545c0e01a060e4e9058f298fa4e3a
PureSlurp <sraymor17@gmail.com>
2024-11-05 09:34:17 -0500

    unlock week 9

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ from fantasy_objects import Season, Week
 import plotly.express as px
 import pandas as pd
 
-WEEK = 8
+WEEK = 9
 
 def get_weeks(week) -> list[Week]:
     '''Returns all the week data up to a given week'''
